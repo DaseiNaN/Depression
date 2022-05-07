9a5e600b4384231de212645fa457eeb0c7c9c9c9
DaseiNaN <dasein_csd@qq.com>
2022-05-07 10:22:04 +0800

    init

---
+++ src/utils/metrics.py
@@ -0,0 +1,37 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+
+def standard_confusion_matrix(y_pred, y_true):
+    """Standard confusion matrix
+
+    ====================================
+    |                |      Truth      |
+    |----------------+-----------------|
+    |                |   True   False  |
+    | Pred  Positive |    TP     FP    |
+    |       Negative |    FN     TN    |
+    ====================================
+    """
+    [[tn, fp], [fn, tp]] = confusion_matrix(y_true, y_pred)
+    return np.array([[tp, fp], [fn, tn]]).astype(np.float32)
+
+
+def measure_performance(y_pred, y_true):
+    y_pred = y_pred.max(dim=1, keepdim=True)[1]
+    y_true = y_true.view_as(y_pred)
+    conf_matrix = standard_confusion_matrix(y_pred.cpu().numpy(), y_true.cpu().numpy())
+    [[tp, fp], [fn, tn]] = conf_matrix
+    accuracy = (tp + tn) / np.sum(conf_matrix)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    return conf_matrix, [accuracy, precision, recall, f1_score]
+
+
+if __name__ == "__main__":
+    import torch
+
+    y_pred = torch.randn((5, 2))
+    print(y_pred)
+    print(y_pred.max(dim=1, keepdim=True)[1])
