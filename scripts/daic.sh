36ca63aab9971e0db8aa39b2e996b02c6a25ee79
DaseiNaN <dasein_csd@qq.com>
2022-06-06 09:56:51 +0800

    update dir structure

--- scripts/daic.sh
+++ scripts/daic.sh
@@ -2,11 +2,15 @@
 # Shedule execution of many runs
 # Run from root folder with: bash scripts/daic.sh
 
-stage=0
+stage=1
 
 if [ $stage -eq 0 ]; then
     echo 'Extracting DAIC-WoZ feats (audio & text)'
     python src/utils/feats/daic_preprocess.py
     python src/utils/feats/daic_feats.py
+fi
+
+if [ $stage -eq 1 ]; then
+    python examples/DAIC-WoZ/cls_train.py
 fi
 
